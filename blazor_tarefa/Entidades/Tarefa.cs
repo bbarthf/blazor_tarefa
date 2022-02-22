aea4263acb6ac0cd4efe3fc640109780ee0f09fc
Brenda Barth Ferreira <B.BarthFerreira@meusburger.com>
2022-02-22 16:38:01 +0100

    Teste segundo push

---
+++ blazor_tarefa/Entidades/Tarefa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazor_tarefa.Entidades
+{
+    public class Tarefa
+    {   // classe do projeto e nao mais local 
+        // git e como se fosse uma sequence
+        public Guid ID { get; set; }
+        public String Descricao { get; set; }
+        public bool Concluida { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+    }
+}
